c4ac36e24ef62fb3bb3b25fddc34579125eb96e4
kmazengia <kefecoder@gmail.com>
2023-05-14 04:49:38 -0700

    web stack debugging

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,4 @@
 # Fix 500 error when a GET HTTP method is requested to Apache web server
 
-exec {'replace':
- provider => shell,
-command => 'sed -i "s/phpp/php/g" /var/www/html/wp-settings.php'
-}
+exec { 'fix-wordpress':
+  command => ('/bin/sed -i "s/phpp/php/g" /var/www/html/wp-settings.php')
